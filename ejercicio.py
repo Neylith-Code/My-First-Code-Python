40c6e4333dee5cf3413f106a94c59d0ebdb82bf0
Neylith-Code <neylithsolanshzavaletasandoval@gmail.com>
2026-01-14 14:54:56 -0500

    My program interactive with input and variables.
    In this exercise I learned to use the input function to interact with the user, I converted text to numbers with int() and I practiced the += operator.

---
+++ ejercicio.py
@@ -0,0 +1,19 @@
+print("Hi, this is my first code in VS Code.")
+print("The result of 66 + 33 is:", 66 + 33)
+print("Other addition.")
+a = 78
+b = 23
+result = a + b
+print("The result of the variables is:", result)
+print("This is so fun")
+a = 54
+a += 16
+print("The next result different addition is:", a)
+print("Finally:")
+kitty = input("What is your name?")
+print("Hi", kitty + "!", "let's add")
+
+number_1 = int(input("Tell me a number:"))
+number_2 = int(input("Tell me a other number"))
+
+print("The addition of your numbers is:", number_1 + number_2)
